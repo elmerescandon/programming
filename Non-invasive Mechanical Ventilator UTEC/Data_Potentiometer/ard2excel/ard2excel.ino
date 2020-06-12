da8f68a7a454556b75f4bcf98a6ef82c255feaa3
Raul Escandon <elmer.escandon@utec.edu.pe>
2020-06-11 23:30:37 -0700

    1

---
+++ Non-invasive Mechanical Ventilator UTEC/Data_Potentiometer/ard2excel/ard2excel.ino
@@ -0,0 +1,19 @@
+int sensorPin = A0;
+int ledPin = 13;  
+int sensorValue = 0; 
+
+void setup() {
+  Serial.begin(115200);
+}
+
+int i = 0;
+void loop() {
+  
+  sensorValue = analogRead(sensorPin);
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(sensorValue);
+  Serial.println(";");
+  i = i + 1;
+
+}
---
+++ Non-invasive Mechanical Ventilator UTEC/Data_Potentiometer/example.py
@@ -0,0 +1,12 @@
+# Obtained from: https://github.com/gsampallo/serialToExcel/blob/master/example.py
+from serialToExcel import SerialToExcel
+
+serialToExcel = SerialToExcel("COM3",115200)
+
+columnas = ["Nro Lectura","Valor"]
+
+serialToExcel.setColumns(["Nro Lectura","Valor"])
+serialToExcel.setRecordsNumber(10000)
+serialToExcel.readPort()
+
+serialToExcel.writeFile("archivo1.xls")
